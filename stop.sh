5a0ee27f7375fdd9dbc306e285b7ebcdd91bf5c5
Michael Prendergast <mikeprendergast@gmail.com>
2018-11-15 13:29:21 -0500

    Add node modules, make stop work

--- stop.sh
+++ stop.sh
@@ -1,1 +1,7 @@
-kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
+#!/bin/bash
+PID=$(ps aux | grep '\snode\s' | awk '{print $2}')
+if [ -n "${PID}" ]
+then
+  echo "Killing node server..."
+  kill -9 ${PID}
+fi
