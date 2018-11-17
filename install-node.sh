dfb72c1f509dddf36d5f7820eaaaa4e8379e43f1
Michael Prendergast <mikeprendergast@gmail.com>
2018-11-17 12:09:05 -0500

    uncomment node installer

--- install-node.sh
+++ install-node.sh
@@ -3,8 +3,8 @@ if ! [ -x "$(command -v nvm)" ]
 then
   #curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
   wget https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh -P /tmp
-  chmod +x /tmp/install.sh
-  /tmp/install.sh
+  #chmod +x /tmp/install.sh
+  source /tmp/install.sh
 fi
 
 if ! [ -x "$(command -v node)" ]
